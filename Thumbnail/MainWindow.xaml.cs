cc46c3a4323239f10b01c9a7437d58ec00a6fb88
VERBENA\tatsu <subs_weavers_0p@icloud.com>
2024-07-16 10:22:51 +0900

    first commit

---
+++ Thumbnail/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using Thumbnail;
+
+namespace Thumbnail
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<ImageItem> _images;
+        public ObservableCollection<ImageItem> Images
+        {
+            get => _images;
+            set
+            {
+                _images = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            Images = new ObservableCollection<ImageItem>();
+            LoadImages();
+        }
+
+        private void LoadImages()
+        {
+            for (int i = 0; i <= 99999; i++)
+            {
+                Images.Add(new ImageItem($"C:\\Temp\\dummy_images2K\\image{i}.jpg"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class ImageItem
+    {
+        public string Uri { get; }
+
+        public ImageItem(string uri)
+        {
+            Uri = uri;
+        }
+    }
+
+    public class ImageLoaderConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string uri)
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(uri, UriKind.Relative);
+                //bitmap.CacheOption = BitmapCacheOption.OnDemand;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.DecodePixelWidth = 200;
+                bitmap.DecodePixelHeight = 100;
+                bitmap.EndInit();
+                bitmap.Freeze();
+                return bitmap;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
